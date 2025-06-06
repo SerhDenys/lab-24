50f16edfe256ce9ec1bb5556a2e9dea2503ce362
SerhDenys <sergdenis@ukr.net>
2025-06-06 13:48:10 +0300

    Edit

--- lab24.c
+++ lab24.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Структура для опису квартири
 typedef struct {
     char street[50];
     int building;
@@ -10,7 +9,6 @@ typedef struct {
     float price;
 } Apartment;
 
-// Функція для введення даних про квартири
 void input_apartments(Apartment *apts, int n) {
     for (int i = 0; i < n; i++) {
         printf("\nКвартира #%d:\n", i+1);
@@ -25,7 +23,6 @@ void input_apartments(Apartment *apts, int n) {
     }
 }
 
-// Функція для оновлення цін квартир
 void update_prices(Apartment *apts, int n) {
     printf("\nОновлення цін квартир:\n");
     for (int i = 0; i < n; i++) {
@@ -36,7 +33,6 @@ void update_prices(Apartment *apts, int n) {
     }
 }
 
-// Функція для виведення даних про квартири
 void print_apartments(Apartment *apts, int n) {
     printf("\nСписок квартир:\n");
     for (int i = 0; i < n; i++) {
@@ -47,75 +43,63 @@ void print_apartments(Apartment *apts, int n) {
 }
 
 int main() {
+    system("chcp 65001");
     int n;
     printf("Введіть кількість квартир: ");
     scanf("%d", &n);
-    
-    // Виділення пам'яті для масиву структур
+
     Apartment *apartments = (Apartment*)malloc(n * sizeof(Apartment));
-    
-    // Введення даних про квартири
+
     input_apartments(apartments, n);
-    
-    // Відкриття файлу для запису у бінарному режимі
+
     FILE *file = fopen("apartments.dat", "wb");
     if (file == NULL) {
         printf("Помилка відкриття файлу для запису!\n");
         free(apartments);
         return 1;
     }
-    
-    // Запис даних у файл за допомогою fwrite
+
     int written = fwrite(apartments, sizeof(Apartment), n, file);
     if (written != n) {
         printf("Помилка запису у файл!\n");
     }
     fclose(file);
-    
-    // Оновлення цін квартир
+
     update_prices(apartments, n);
-    
-    // Відкриття файлу для оновлення даних
+
     file = fopen("apartments.dat", "r+b");
     if (file == NULL) {
         printf("Помилка відкриття файлу для оновлення!\n");
         free(apartments);
         return 1;
     }
-    
-    // Оновлення даних у файлі
+
     for (int i = 0; i < n; i++) {
-        // Перехід до потрібної позиції у файлі
+
         fseek(file, i * sizeof(Apartment), SEEK_SET);
-        
-        // Запис оновлених даних
+
         fwrite(&apartments[i], sizeof(Apartment), 1, file);
     }
-    
-    // Закриття файлу
+
     fclose(file);
-    
-    // Відкриття файлу для читання та перевірки результатів
+
     file = fopen("apartments.dat", "rb");
     if (file == NULL) {
         printf("Помилка відкриття файлу для читання!\n");
         free(apartments);
         return 1;
     }
-    
-    // Читання даних з файлу за допомогою fread
+ 
     Apartment *read_apartments = (Apartment*)malloc(n * sizeof(Apartment));
     int read = fread(read_apartments, sizeof(Apartment), n, file);
     if (read != n) {
         printf("Помилка читання з файлу!\n");
     }
     fclose(file);
-    
-    // Виведення оновлених даних
+
     printf("\nОновлені дані з файлу:\n");
     print_apartments(read_apartments, n);
-    
-    // Звільнення пам'яті
+
     free(apartments);
     free(read_apartments);
     
